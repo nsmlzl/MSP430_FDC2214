2b045f6881a7b4e96341059b90291f2f86f24a7d
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2017-11-17 17:44:39 +0100

    added blinking and Header

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
+/*
+ *	Written by Niklas Schmelzle <niklas.schmelzle@gmail.com>
+ *	main.c
+ *
+ *	TODO:
+ *
+ */
+
 #include <msp430f2274.h>
 
 
-/**
- * main.c
- */
+void delay(){
+	for(int i = 0; i < 50; i++){
+		__delay_cycles(25000);
+	}
+}
+
+
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 
 	P1DIR = 0xFF;
-	P1OUT = 0xFF;
+	while(1){
+		P1OUT = 0b1;
+		delay();
+		P1OUT = 0b10;
+		delay();
+	}
 
 	return 0;
 }
