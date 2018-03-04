e76f34659db04229dd6c6b49484fef44f740f1f4
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2018-03-04 20:59:35 +0100

    added timer interrupts for x-axis milliseconds counter and measurement intervalls

--- main.c
+++ main.c
@@ -25,6 +25,12 @@ void greenLed();
 void ledOn();
 void ledOff();
 
+// timer
+void timerStart(uint16_t measurementPeriod);
+void timerStop();
+
+volatile uint32_t milliSeconds = 0;
+
 
 uint16_t main(void){
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
@@ -35,6 +41,7 @@ uint16_t main(void){
 /*
  * testing fdc
  */
+	/*
 	uint8_t err = 0;
 	uint32_t tmpCapacity = 0;
 	err += nc_init();
@@ -42,9 +49,20 @@ uint16_t main(void){
 	while(1){
 
 		tmpCapacity = 0;
-		err += nc_get_capacity(&tmpCapacity, 3);
+		err += nc_get_capacity(&tmpCapacity, 0);
 
 	}
+	*/
+
+/*
+ * testing timer
+ */
+
+	timerStart(1000);
+	__bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, enable interrupts
+	__bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, enable interrupts
+	__bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, enable interrupts
+	timerStop();
 
 	ledOff();
 	return 0;
@@ -87,4 +105,47 @@ void ledOn(){
 void ledOff(){
 	P1OUT &= ~0x1;
 	P4OUT &= ~(0x1 << 7);
+}
+
+
+// timer init for x-axis and intervall measurement timer
+void timerStart(uint16_t measurementPeriod){
+	milliSeconds = 0;
+	// timer A0.0 for x-axis
+	TA0CCTL0 = CCIE;
+	TA0CCR0 = 32;
+	// timer A1.0 for measurement intervall
+	if(measurementPeriod > 2047){
+		// else overflow would happen
+		measurementPeriod = 2047;
+	}
+	TA1CCTL0 = CCIE;
+	TA1CCR0 = 32 * measurementPeriod;
+
+	// start timer
+	TA0CTL = TASSEL_1 + ID_0 + MC_1 + TACLR;
+	TA1CTL = TASSEL_1 + ID_0 + MC_1 + TACLR;
+}
+
+
+// stop timer A0 and A1 (both timers)
+void timerStop(){
+	TA0CCTL0 &= ~CCIE;
+	TA1CCTL0 &= ~CCIE;
+}
+
+// timer interrupt for x-axis (counts milliSeconds)
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void TIMER0_A0_ISR(){
+	milliSeconds++;
+}
+
+
+// timer interrupt for intervall between measurements
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void TIMER1_A0_ISR(){
+	// restart after x milliseconds (according to setup of TA0CCR1)
+	__bic_SR_register_on_exit(LPM0_bits);
+
+	// check if old measurement wasn't completed -> error
 }
